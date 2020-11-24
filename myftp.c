8a18f8079e867ef7383271be6a5f2097ec3cb9dd
SamBater <sam505@21cn.com>
2020-11-25 00:45:22 +0800

    update

--- myftp.c
+++ myftp.c
@@ -1,4 +1,9 @@
 #include "shaderd_function.c"
+#include <sys/socket.h> 
+#include <error.h>
+#define MAX 80 
+#define PORT 8080
+#define SA struct sockaddr
 
 //将远程用户、服务器信息读入
 login_info* check_cmd(char*argv)
@@ -17,36 +22,76 @@ login_info* check_cmd(char*argv)
 		cmd->port = port;
 		cmd->user = user;
 		cmd->pwd = pwd;
+		printf("cmd->pwd = %s\n",pwd);
 		return cmd;
 	}
 	printf("connecction error!\n");
 	return NULL;
 }
 
-int main(int argc,char* argv[])
-{
-	struct sockaddr_in server_sockaddr;
-	server_sockaddr.sin_family = AF_INET;
-
-	if(argc == 2)
+ 
+void func(int sockfd,login_info* info) 
+{ 
+    char buff[MAX]; 
+	bzero(buff,sizeof(buff));
+	write(sockfd,info->user,sizeof(info->user));
+	write(sockfd,info->pwd,sizeof(info->pwd));
+	while(read(sockfd,buff,sizeof(buff))!=0)
 	{
-		login_info* info = check_cmd(argv[1]);
-		if(info != NULL)
-		{
-			server_sockaddr.sin_addr.s_addr = inet_addr(info->ip);
-			server_sockaddr.sin_port = atoi(info->port);
-		}
-		{
-			exit(0);
-		}
+		puts(buff);
 	}
 
-	int server_sockfd;
-	if(server_sockfd =  socket(AF_INET,SOCK_STREAM,0) == -1)
+} 
+
+int main(int argc,char** argv) 
+{ 
+	login_info* info = NULL;
+	if(argc != 2)
 	{
-		fprintf(stderr,"Socket error:%s\n\a", strerror(errno));
-		exit(1);
+		printf("useage:ftpclient <user>:<passwd>@<host>:<port>\n");
+		exit(0);
 	}
-	bzero(&server_sockaddr,sizeof(struct sockaddr_in));
-}
+
+	if((info = check_cmd(argv[1])) == NULL)
+	{
+		printf("useage:ftpclient <user>:<passwd>@<host>:<port>\n");
+		exit(0);
+	}
+
+	int sockfd, connfd; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and varification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = inet_addr(info->ip); 
+	servaddr.sin_port = htons(8080);
+
+	extern int erron;
+	errno = 0;
+	// connect the client socket to server socket 
+	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) 
+	{ 
+		printf("connection with the server failed...\n"); 
+		printf("%s\n",strerror(errno));
+		exit(0); 
+	} 
+	else
+		printf("connected to the server..\n"); 
+
+	// function for chat 
+	func(sockfd,info); 
+
+	// close the socket 
+	close(sockfd); 
+} 
 
--- myserver.c
+++ myserver.c
@@ -2,6 +2,16 @@
 #include <pwd.h>
 #include <shadow.h>
 #include <crypt.h>
+#include <stdio.h> 
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#define MAX 80 
+#define PORT 8080
+#define SA struct sockaddr 
 
 #include "shaderd_function.c"
 /// @return 0 - password is correct, otherwise no need root permision
@@ -33,10 +43,87 @@ int CheckPassword( const char* user, const char* password )
 }
 
 
-int main(int argc,char* argv[])
-{
-    if(argc == 2)
-    {
-        
-    }
-}
+
+
+// Function designed for chat between client and server. 
+void func(int sockfd) 
+{ 
+	char buff[MAX]; 
+	char user[MAX];
+	char pwd[MAX];
+	bzero(buff,MAX);
+	bzero(user,MAX);
+	bzero(pwd,MAX);
+
+	read(sockfd,user,sizeof(user));
+
+	read(sockfd,pwd,sizeof(pwd));
+
+	printf("user:%s\n",user);
+	printf("passwd:%s\n",pwd);
+	if(CheckPassword(user,"w1ww11w123") == 0)
+	{
+		strncpy(buff,"login in,wealcome!\n",sizeof(buff));
+		write(sockfd,buff,sizeof(buff));
+		printf("yes!\n");
+	}
+	else
+	{
+		printf("error.\n");
+	}
+	
+} 
+
+// Driver function 
+int main() 
+{ 
+	int sockfd, connfd, len; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and verification 
+	sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+	if (sockfd == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	servaddr.sin_port = htons(PORT);
+
+	// Binding newly created socket to given IP and verification 
+	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+		printf("socket bind failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n"); 
+
+	// Now server is ready to listen and verification 
+	if ((listen(sockfd, 5)) != 0) { 
+		printf("Listen failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Server listening..\n"); 
+	len = sizeof(cli); 
+
+	// Accept the data packet from client and verification 
+	connfd = accept(sockfd, (SA*)&cli, &len); 
+	if (connfd < 0) { 
+		printf("server acccept failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("server acccept the client...\n"); 
+
+	// Function for chatting between client and server 
+	func(connfd); 
+
+	// After chatting close the socket 
+	close(sockfd); 
+} 
